af1080af835da5f024577821f8ac3add1c31eadf
Max Garcia <androimspiegel@gmail.com>
2020-05-03 01:43:56 -0500

    mis primeros archivos

---
+++ src/main/java/maxi/com/App.java
@@ -0,0 +1,15 @@
+package maxi.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String firstName;
+        firstName = "Hola Max Garcia";
+        System.out.println(firstName);
+    }
+}
---
+++ src/main/java/maxi/com/Max.java
@@ -0,0 +1,9 @@
+package maxi.com;
+
+public class Max {
+    public static void main(String[] args) {
+        String lastName;
+        lastName = "Shapiama";
+        System.out.println(lastName);
+    }
+}
